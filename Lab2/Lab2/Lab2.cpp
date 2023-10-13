c596d43019ddd79866edfac5ddc127ad0dea5961
Lia Fisher <willfisher1007@hotmail.ca>
2023-10-13 10:12:24 -0400

    SDL
    Added the SDL files and configured the project to use them

--- Lab2/Lab2/Lab2.cpp
+++ Lab2/Lab2/Lab2.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include "Engine.h"
 
+#define SDL_MAIN_HANDLED
+#include "SDL.h"
+#include "SDL_image.h"
+
 int main()
 {
     Engine myengine = Engine();
-    //myengine.Initialize();
+
+    //////////////////////////////////////////
+
+
+    /*SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+    window = SDL_CreateWindow("SDL Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_DestroyWindow(window);
+    SDL_Quit();*/
 }
